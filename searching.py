09c5ec48b64a6101e01339c7aab7a5e824ac2eb2
Wansen Dong <wensen.dong@gmail.com>
2018-04-07 19:42:41 +1000

    Update searching.py

--- searching.py
+++ searching.py
@@ -1,7 +1,7 @@
 ## Variables
 inputs_so_far = 0
 ## Functions
-def get_input(inputs_already, typeof=None):
+def get_input(prompt, inputs_already):
   class String(object):
     def __init__(self, string):
       self.string = string
@@ -12,11 +12,23 @@ def get_input(inputs_already, typeof=None):
         try:
           return int(self.string)
         except:
-          pass
-  if typeof == None:
-    typeof = int
-  pass
-  if typeof == int:
-    return typeof
-  elif typeof == str:
-    return String(typeof).integer()
+          raise ValueError("Invalid content in self.string")
+  typeof = raw_input(prompt)
+  try:
+    return [String(typeof).integer()+1, typeof]
+  except:
+    the_list = list(str(String(typeof).integer()))
+    the_list.pop()
+    the_list.pop()
+    return ["".join(the_list), typeof]
+    
+## How to use get_input():
+## a_list = get_input(<put prompt here>, inputs_so_far)
+## inputs_so_far = a_list[0]
+## user_returned_text = a_list[1]
+
+## Input and Output
+while True:
+  a_list = get_input("Welcome to the searching page. To search, type something in!", inputs_so_far)
+  inputs_so_far = a_list[0]
+  text = a_list[1]
